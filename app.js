da35568b7753dcd15c8459495bb9dba7c6a4697f
Youngjae Kim <2015fromjeki@gmail.com>
2024-05-15 17:19:19 +1000

    final Edit

--- app.js
+++ app.js
@@ -45,67 +45,67 @@
 //   addForm.add.value = "";
 // });
 
-//delegation form
-//delete todos
+//Add
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
 
-// ul.addEventListener("click", (e) => {
-//   if (e.target.classList.contains("delete")) {
-//     e.target.parentElement.remove();
-//   }
-// });
-
-// ul.addEventListener("click", (e) => {
-//   if (e.target.classList.contains("delete")) {
-//     e.target.parentElement.remove();
-//   }
-// });
-
-const ul = document.querySelector(".list-group");
-//Adding function
-const textGenerate = (todo) => {
-  ul.innerHTML += ` <li
+const generateTemplate = (todo) => {
+  list.innerHTML += `<li
     class="list-group-item d-flex justify-content-between align-items-center"
   >
     <span>${todo}</span>
     <i class="far fa-trash-alt delete"></i>
   </li>`;
 };
-
-const addForm = document.querySelector(".add");
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const todo = addForm.add.value.trim();
-  if (addForm.add.value == "") {
-    alert("Please Write Something");
+  if (e.target.add.value == "") {
+    alert("Please write down Something");
   } else {
-    textGenerate(todo);
+    generateTemplate(e.target.add.value);
+    e.target.add.value = "";
   }
-  addForm.add.value = "";
 });
-
-//delete Function
-//delegation Form
-ul.addEventListener("click", (value) => {
-  if (value.target.classList.contains("delete")) {
-    value.target.parentElement.remove();
+//Delete
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
   }
 });
 
 //Search Function
-const search = document.querySelector(".search input");
+// const search = document.querySelector(".search input");
 
-const filterTodos = (term) => {
-  Array.from(ul.children)
+// const filterTodos = (term) => {
+//   Array.from(list.children)
+//     .filter((todo) => !todo.textContent.includes(term))
+//     .forEach((todo) => todo.classList.add("filtered"));
+
+//   Array.from(list.children)
+//     .filter((todo) => todo.textContent.includes(term))
+//     .forEach((todo) => todo.classList.remove("filtered"));
+// };
+
+// //keyup event
+// search.addEventListener("keyup", () => {
+//   const term = search.value.trim().toLowerCase();
+//   filterTodos(term);
+// });
+
+//Search Funciton
+
+const filterTodo = (term) => {
+  Array.from(list.children)
     .filter((todo) => !todo.textContent.includes(term))
     .forEach((todo) => todo.classList.add("filtered"));
 
-  Array.from(ul.children)
+  Array.from(list.children)
     .filter((todo) => todo.textContent.includes(term))
     .forEach((todo) => todo.classList.remove("filtered"));
 };
 
-//keyup event
-search.addEventListener("keyup", () => {
-  const term = search.value.trim().toLowerCase();
-  filterTodos(term);
+const form = document.querySelector(".search");
+form.addEventListener("input", (e) => {
+  const searching = e.target.value;
+  filterTodo(searching);
 });
